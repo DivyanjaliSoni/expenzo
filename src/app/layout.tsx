3a6a3a3eed66cf9aa7a7b659d5490774282447c8
Divyanjali Soni <divyanjali@360adaptive.com>
2024-10-24 12:29:20 +0530

    removed store provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { useState } from "react";
 import { FaRegLightbulb } from "react-icons/fa6";
 import { FaLightbulb } from "react-icons/fa6";
 import { Provider } from "react-redux";
-import { store } from "../store/store";
 import { AiOutlineLogout } from "react-icons/ai";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
@@ -29,7 +28,6 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={nunito.className}>
-        <Provider store={store}>
           <header className="fixed top-0 w-full p-5 dark:bg-gray-700 dark:text-white bg-white border-b text-gray-800">
             <div className="flex justify-between items-center">
               <div className="text-2xl [text-shadow:1px_1px_2px_#565656]">
@@ -56,7 +54,6 @@ export default function RootLayout({
             </div>
           </header>
           <main className="mt-[71px]">{children}</main>
-        </Provider>
       </body>
     </html>
   );
