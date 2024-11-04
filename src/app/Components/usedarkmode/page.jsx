d1fcbe1656e2e967ba9117785044f80ea2ff12fe
Divyanjali Soni <divyanjali@360adaptive.com>
2024-11-04 18:06:49 +0530

    try again

--- src/app/Components/usedarkmode/page.jsx
+++ src/app/Components/usedarkmode/page.jsx
@@ -2,23 +2,25 @@
 import { useEffect, useRef } from "react";
 
 const useDarkMode = () => {
-  const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-  const isDarkModeRef = useRef(mediaQuery.matches); // Use ref to hold the current value
+  const isDarkModeRef = useRef(false); // Use ref to hold the current value
 
   useEffect(() => {
-    const updateDarkMode = (e) => {
-      isDarkModeRef.current = e.matches; // Update the ref value
-    };
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
     // Set initial value based on media query
     isDarkModeRef.current = mediaQuery.matches;
 
+    const updateDarkMode = (e) => {
+      isDarkModeRef.current = e.matches; // Update the ref value
+    };
+
     mediaQuery.addEventListener("change", updateDarkMode);
 
+    // Cleanup function to remove the event listener
     return () => {
       mediaQuery.removeEventListener("change", updateDarkMode);
     };
-  }, []);
+  }, []); // The effect runs only once on mount
 
   return isDarkModeRef.current; // Return the current value directly
 };
