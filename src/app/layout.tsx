4a38efaded054cdc7a25f981ce920a46f7aef882
Divyanjali Soni <divyanjali@360adaptive.com>
2024-11-04 12:44:59 +0530

    Enabled  PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,76 +1,37 @@
-"use client";
 import "./globals.css";
 import { Nunito } from "next/font/google";
-import { useEffect, useState } from "react";
-import { FaRegLightbulb } from "react-icons/fa6";
-import { FaLightbulb } from "react-icons/fa6";
-import { AiOutlineLogout } from "react-icons/ai";
-import Cookies from "js-cookie";
-import { usePathname, useRouter } from "next/navigation";
+import type { Metadata, Viewport } from "next";
+import Header from "./Components/header/page";
 
 const nunito = Nunito({
   subsets: ["latin"],
 });
+export const metadata: Metadata = {
+  manifest: "/manifest.json",
+  title: "Expenso",
+  description: "Track your expenses efficiently.",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  const pathname = usePathname();
-  const router = useRouter();
-
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  const checkAuth = () => {
-    const token = Cookies.get("authToken");
-    setIsAuthenticated(!!token);
-  };
-
-  useEffect(() => {
-    checkAuth();
-  }, [pathname]); 
-
-  const handleLogout = () => {
-    Cookies.remove("authToken");
-    Cookies.remove("authUserId");
-    setIsAuthenticated(false);
-    router.push("/login");
-  };
-
   return (
-    <html lang="en" className={isDarkMode ? "dark" : ""}>
-      <head>
-        <title>Expenso</title>
-        <meta name="description" content="Track your expenses efficiently." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
+    <html lang="en">
+      <head></head>
       <body className={nunito.className}>
-        <header className="fixed top-0 w-full p-5 dark:bg-gray-700 dark:text-white bg-white border-b text-gray-800">
-          <div className="flex justify-between items-center">
-            <div className="text-2xl [text-shadow:1px_1px_2px_#565656]">
-              EXPENso
-            </div>
-            <div className="flex gap-4 justify-center items-center">
-              <div
-                className="text-2xl cursor-pointer"
-                onClick={() => setIsDarkMode(!isDarkMode)}
-              >
-                {isDarkMode ? <FaRegLightbulb /> : <FaLightbulb />}
-              </div>
-              {isAuthenticated && (
-                <div
-                  className="text-2xl cursor-pointer"
-                  onClick={() => handleLogout()}
-                >
-                  <AiOutlineLogout />
-                </div>
-              )}
-            </div>
-          </div>
-        </header>
+        <Header />
         <main className="mt-[71px]">{children}</main>
       </body>
     </html>
